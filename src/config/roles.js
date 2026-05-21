1241441ed260929f71bd9136953147bffdb8a153
Nguyen Duc Khanh <khanhhd8880@gmail.com>
2026-05-21 23:38:30 +0700

    feat: them quyen

--- src/config/roles.js
+++ src/config/roles.js
@@ -1,19 +1,68 @@
+const publicRights = [
+    'getProducts',
+    'getCategories',
+    'getProductVariants',
+    'getReviews',
+];
+
+const userRights = [
+    ...publicRights,
+    'getProfile',
+    'manageProfile',
+    'getBookings',
+    'manageBookings',
+    'getWallet',
+    'manageWallet',
+    'getFavorites',
+    'manageFavorites',
+    'getCollections',
+    'manageCollections',
+    'getNotifications',
+    'manageNotifications',
+    'useChatbot',
+];
+
+const plusRights = [
+    ...userRights,
+    'useAiTryOn',
+    'useVisualSearch',
+    'getAiUsage',
+];
+
+const proRights = [
+    ...plusRights,
+];
+
+const adminRights = [
+    ...proRights,
+    'admin',
+    'getUsers',
+    'manageUsers',
+    'manageCategories',
+    'manageProducts',
+    'manageProductVariants',
+    'getInventory',
+    'manageInventory',
+    'getAllBookings',
+    'manageAllBookings',
+    'getAllWallets',
+    'manageAllWallets',
+    'getPayments',
+    'managePayments',
+    'getSubscriptions',
+    'manageSubscriptions',
+    'getAiUsage',
+    'manageAiUsage',
+    'manageReviews',
+    'getDashboard',
+    'manageDashboard',
+];
+
 const allRoles = {
-    user: [
-        'admin'
-    ],
-
-    plus: [
-        'admin'
-    ],
-
-    pro: [
-        'admin'
-    ],
-
-    admin: [
-        'admin'
-    ],
+    user: userRights,
+    plus: plusRights,
+    pro: proRights,
+    admin: adminRights,
 };
 
 const roles = Object.keys(allRoles);
